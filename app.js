d8d2e3f65c865fff61f33451c56971e08e50b025
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-09-02 13:53:15 +0900

    [Add] 足し算

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
  let hello = "Hello, World"; 
  alert(hello);
  
+ //【変数に代入できるコードの例】
+ 
  // 整数を代入する 
 let int1 = 1;
 alert(int1);
@@ -13,6 +15,20 @@ alert(int2);
 let float1 = 3.14;
 alert(float1);
 
+//【四則演算の書き方】
+
 // 文字列を代入する 
 let str1 = 'JavaScriptを覚えよう';
 alert(str1);
+
+// 足し算 
+alert(4 + 3);
+
+// 引き算
+alert(8 - 5);
+
+// 掛け算
+alert(2 * 6);
+
+// 割り算
+alert(10 / 2);
