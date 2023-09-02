97de690410c5b96bf625562b6192db5e5f2e7088
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-09-02 15:28:55 +0900

    [Add] 条件分岐

--- app.js
+++ app.js
@@ -39,3 +39,15 @@ alert('Hello' + 'World');
 let str2 = 'Hello';
 let str3 = 'World!!';
 alert(str2 + str3);
+
+//【条件分岐】
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
