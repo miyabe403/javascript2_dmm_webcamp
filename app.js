5ebe8e18394a9d07a3bcf2de727f327fc828f276
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-09-02 16:08:37 +0900

    [Add] 繰り返し処理

--- app.js
+++ app.js
@@ -51,3 +51,32 @@ if(orange < apple){
 } else{
   alert('みかんの値段がりんごより高い');
 }  
+// 条件1を満たすときは処理1、条件2を満たすときは処理2、どちらも満たさないときは処理3を実行
+
+//【繰り返し処理】
+//【while文の記述】
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num < max){
+  num = num * 2;
+  count = count + 1;
+}
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+//【do...while文の記述】
+// do{
+//   最低1回は行われる処理
+// }while(条件式);
+
+//【for文の記述】
+let i;
+let num2 = 0;
+
+for(i = 1; i < 11; i++){
+  num2 = num2 + i;
+}
+
+alert('1から10まで足し算した結果は' + num + 'です');
