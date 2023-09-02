efff46b0ea83583aee57d0cd682b33ed77ae3feb
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-09-02 20:01:17 +0900

    [Add] varによる再宣言を実行

--- js_practice_variable/sample.js
+++ js_practice_variable/sample.js
@@ -1,5 +1,37 @@
-// varによる再宣言 
-var nickname = "taro"
+//【再宣言】
+// varによる再宣言  
+// var nickname = "taro"
+// console.log(nickname)
+// var nickname = "ichiro"
+// console.log(nickname)
+
+// letによる再宣言
+// let nickname = "taro"
+// console.log(nickname)
+// let nickname = "ichiro"
+// console.log(nickname)
+
+// constによる再宣言
+// const nickname = "taro"
+// console.log(nickname)
+// const nickname = "ichiro"
+// console.log(nickname)
+
+//【再代入】
+// varによる再代入
+// var nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+
+// letによる再代入
+// let nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
+
+// constによる再代入
+const nickname = "taro"
 console.log(nickname)
-var nickname = "ichiro"
+nickname = "jiro"
 console.log(nickname)
