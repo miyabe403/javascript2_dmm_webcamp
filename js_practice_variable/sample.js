7986e2df519d38b3474cfffae01fd1cb9e30f690
miyabe403 <108040533+miyabe403@users.noreply.github.com>
2023-09-02 21:47:03 +0900

    [Add] letのスコープ

--- js_practice_variable/sample.js
+++ js_practice_variable/sample.js
@@ -31,20 +31,33 @@
 // console.log(nickname)
 
 // constによる再代入 
-const nickname = "taro"
-console.log(nickname)
-nickname = "jiro"
-console.log(nickname)
+// const nickname = "taro"
+// console.log(nickname)
+// nickname = "jiro"
+// console.log(nickname)
 
 //【スコープ】   
 // varのスコープ 
 // varでは関数スコープ（ローカルスコープ）、グローバルスコープの変数を宣言できます。
-var str = "webcamp" //グローバルスコープ
+// var str = "webcamp" //グローバルスコープ
 
+// function foo() {
+//   console.log(str)
+//   var y = "hello" //関数スコープ
+// }
+
+// foo()
+// console.log(y)
+
+// letのスコープ
+// letではブロックスコープ（ローカルスコープ）の変数を宣言
 function foo() {
-  console.log(str)
-  var y = "hello" //関数スコープ
+  let x = "webcamp"
+  {
+    let y = "hello webcamp"
+  }
+  console.log(x)
+  console.log(y)
 }
 
 foo()
-console.log(y)
